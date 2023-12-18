3b4beaf6a78cbc253a93d32278ea8781ff3451b6
FlaSH514 <amitbabaninayak@gmail.com>
2023-12-18 21:08:22 +0530

    Error

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,36 +1,50 @@
+// import GoogleProvider from "next-auth/providers/google";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// export const options = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         username: {
+//           label: "Username",
+//           type: "text",
+//           placeholder: "Your username...",
+//         },
+//         password: {
+//           label: "Password",
+//           type: "password",
+//           placeholder: "Your password...",
+//         },
+//       },
+//       async authorize(credentials) {
+//         const user = { id: "42", name: "Dave", password: "next" };
+//         if (
+//           credentials?.username === user.name &&
+//           credentials?.password === user.password
+//         ) {
+//           return user;
+//         } else {
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
+// };
+
+// import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+
 export const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "Your username...",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Your password...",
-        },
-      },
-      async authorize(credentials) {
-        const user = { id: "42", name: "Dave", password: "next" };
-        if (
-          credentials?.username === user.name &&
-          credentials?.password === user.password
-        ) {
-          return user;
-        } else {
-          return null;
-        }
-      },
+      clientSecret: process.env.GOOGLE_SECRET_ID,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  logger: { level: "debug" },
 };
